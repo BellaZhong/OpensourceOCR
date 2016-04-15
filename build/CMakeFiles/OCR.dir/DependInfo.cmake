04a58f89d1bd7729b63b413c925405c11697c4bb
Bella <misszhongy@126.com>
2016-04-15 03:57:42 -0700

    opensourceOCR

---
+++ build/CMakeFiles/OCR.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/zhong/basicOCR-master/basicOCR/basicOCR.cpp" "/home/zhong/basicOCR-master/build/CMakeFiles/OCR.dir/basicOCR.o"
+  "/home/zhong/basicOCR-master/basicOCR/main.cpp" "/home/zhong/basicOCR-master/build/CMakeFiles/OCR.dir/main.o"
+  "/home/zhong/basicOCR-master/basicOCR/preprocessing.cpp" "/home/zhong/basicOCR-master/build/CMakeFiles/OCR.dir/preprocessing.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ build/CMakeFiles/OCR.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OCR.dir/main.o"
+  "CMakeFiles/OCR.dir/basicOCR.o"
+  "CMakeFiles/OCR.dir/preprocessing.o"
+  "OCR.pdb"
+  "OCR"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OCR.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
